502be808ebebe06f085a05dea84bd0806ed032b9
cemilcan0 <05cemilcan@gmail.com>
2023-08-05 01:56:27 +0300

    some fix

--- Array String/238. Product of Array Except Self.py
+++ Array String/238. Product of Array Except Self.py
@@ -3,14 +3,14 @@ from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        çarp = [1] * n
+        array = [1] * n
         left = right = 1
         for i in range(n):
-            çarp[i] *= left
+            array[i] *= left
             left *= nums[i]
         
         for i in range(n-1, -1, -1):
-            çarp[i] *= right
+            array[i] *= right
             right *= nums[i]
         
-        return çarp
+        return array
